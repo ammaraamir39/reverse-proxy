e10b8152ea8fa6f33d89a24eceb9613d52ee170c
ammaraamir39 <ammaraamir253@gmail.com>
2024-12-22 17:42:24 -0500

    Added config

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,8 @@
 import fs from "node:fs/promises"
 import { parse } from "yaml"
 
-/**Reading the file contents and then converting the yaml to the JSON and then returning it as string */
+/**Reading the file contents and then converting the yaml to the JSON
+     and then returning it as string */
 async function parseYAMLConfig(filePath: string) {
   const configFileContent = await fs.readFile(filePath, "utf8")
   const configurationParsed = parse(configFileContent)
